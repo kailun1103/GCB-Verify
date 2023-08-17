d39758f68bbfdd254cebaf8a1e4dbd057dacc489
kailun1103 <kailunchang1103@gmail.com>
2023-08-17 21:37:17 +0800

    92-96 program codes add explanation comment

--- TWGCB-01-008-0093_Check.sh
+++ TWGCB-01-008-0093_Check.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+# 設定 chrony 配置文件的路徑
 file_path="/etc/chrony.conf"
+# 定義檢查閾值（原有文件裡server以及pool的數量）
 server_threshold=2
 pool_threshold=5
 
+# 檢查配置文件是否存在
 if [ -f "$file_path" ]; then
+    # 計算配置文件中 'server' 關鍵字的出現次數
     server_count=$(grep -oE "server" "$file_path" | wc -l)
+    # 計算配置文件中 'pool' 關鍵字的出現次數
     pool_count=$(grep -oE "pool" "$file_path" | wc -l)
 
+    # 檢查server以及pool的數量是否超過閾值
     if [ "$server_count" -gt "$server_threshold" ] || [ "$pool_count" -gt "$pool_threshold" ]; then
-        echo ""chrony.conf" of TWGCB-01-008-0093 has been configured for NTP."
+        echo "chrony.conf" of TWGCB-01-008-0093 has been configured for NTP."
     else
-        echo ""chrony.conf" of TWGCB-01-008-0093 has not been configured for NTP."
+        echo "chrony.conf" of TWGCB-01-008-0093 has not been configured for NTP."
     fi
 else
     echo "The file does not exist."
--- TWGCB-01-008-0096_Check_2.sh
+++ TWGCB-01-008-0096_Check_2.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# 設定 snmpd.conf 配置文件的路徑
 config_file="/etc/snmp/snmpd.conf"
+# 要檢查的SNMPv3用戶
 user_strings=("rouser" "rwuser")
+# 目標行數設定
 target_strings=("com2sec notConfigUser  default       public" 
                 "group   notConfigGroup v1           notConfigUser"
                 "group   notConfigGroup v2c           notConfigUser"
@@ -9,10 +12,11 @@ target_strings=("com2sec notConfigUser  default       public"
                 "view    systemview    included   .1.3.6.1.2.1.25.1.1"
                 "access  notConfigGroup \"\"      any       noauth    exact  systemview none none")
 
-found=0
-uncommented=0
-user_count=0  # 用來記錄找到的用戶數量
+found=0          # 用於記錄是否找到符合 target_strings 的行
+uncommented=0    # 用於記錄未被註解的行數
+user_count=0     # 用於記錄找到的 SNMPv3 用戶數量
 
+# 判斷有幾個SNMPv3用戶
 while IFS= read -r line; do
     for user_target in "${user_strings[@]}"; do
         if [[ $line == *"$user_target"* ]]; then
@@ -22,6 +26,7 @@ while IFS= read -r line; do
     done
 done < "$config_file"
 
+# 判斷有幾個符合的目標行
 while IFS= read -r line; do
     for target in "${target_strings[@]}"; do
         if [[ $line == *"$target"* ]]; then
@@ -34,14 +39,15 @@ while IFS= read -r line; do
     done
 done < "$config_file"
 
+# 根據找到的情況輸出訊息
 if [[ $found -eq 1 ]]; then
     if [[ $uncommented -eq 1 ]]; then
-        echo "Configuration of SNMPv3 for TWGCB-01-008-0096_Check_2.sh has failed."
+        echo "Configuration of SNMPv3 for TWGCB-01-008-0096 has failed."
     else
         if [[ $user_count -gt 0 ]]; then
-            echo "Configuration of SNMPv3 for TWGCB-01-008-0096_Check_2.sh has been completed."
+            echo "Configuration of SNMPv3 for TWGCB-01-008-0096 has been completed."
         else
-            echo "Configuration of SNMPv3 for TWGCB-01-008-0096_Check_2.sh has failed."
+            echo "Configuration of SNMPv3 for TWGCB-01-008-0096 has failed."
         fi
     fi
 fi
